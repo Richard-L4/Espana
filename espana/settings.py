4eaa820371c4e345e5b12bdf19cfed92263bb7db
Richard-L4 <rwells945@gmail.com>
2026-02-02 16:29:37 +0000

    remove a comment from settings

--- espana/settings.py
+++ espana/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECRET_KEY = 'django-insecure-xv30d2fsw%g&t7pv3fd*ko^03ll4t0=lz2(n@so)r3z87mi9g$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
