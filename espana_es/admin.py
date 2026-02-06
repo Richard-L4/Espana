5efedfb00a64a1a11b94da2cc461d05bbef35067
Richard-L4 <rwells945@gmail.com>
2026-02-06 15:47:29 +0000

    title added to cards in cities page and more detail in display in django admin

--- espana_es/admin.py
+++ espana_es/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Contact, CardText
 
+
 # Register your models here.
 
 # -------------
 # Contact
 # -------------
-admin.site.register(Contact)
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
+    list_display = ('name', 'email')
 
 
 # --------------
@@ -15,4 +18,8 @@ admin.site.register(Contact)
 
 @admin.register(CardText)
 class CardTextAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'short_content', 'image_name')
+
+    def short_content(self, obj):
+        return obj.content[:50] + ("..." if len(obj.content) > 50 else "")
+    short_content.short_description = "Content"
